d2f759460e4c80f9819ad86e329fb2a087db9404
Angelina <atvorngsam@gmail.com>
2022-12-21 21:40:29 -0600

    Thought Form Component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2022 by Angelina Vorngsam</div>
+      <div className="container">
+        &copy;{new Date().getFullYear()} by Angelina Vorngsam
+      </div>
     </footer>
   );
 };
